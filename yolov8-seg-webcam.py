d3e2da5af821a430730b050911ba592401453078
BYUNG <45321943+ilovebyung@users.noreply.github.com>
2024-07-03 12:17:16 +0900

    Update yolov8-seg-webcam.py
    camera detects segmented objects and calculate area

--- yolov8-seg-webcam.py
+++ yolov8-seg-webcam.py
@@ -4,33 +4,7 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-model = YOLO('yolov8s-seg.pt')
-
-
-'''
-image 
-'''
-image = Image.open("dog.jpg")
-
-# results = model.predict(source=image, save=True)  # save plotted images
-results = model.predict(source=image)  
-
-for result in results:
-    # Segmentation
-    data = result.masks.data      # masks, (N, H, W)
-    mask = data[0]
-    mask = mask.cpu()*255
-    mask = mask.numpy()
-    mask = mask.astype(np.uint8)
-    # num_zeros = np.count_nonzero(mask == 0)
-    detected_area = np.count_nonzero(mask == 255)
-    # print(num_zeros)
-    print(detected_area)
-
-
-'''
-webcam 
-'''
+model = YOLO('yolov8m-seg.pt')
 
 # Initialize the camera
 cap = cv2.VideoCapture(0)
@@ -38,39 +12,44 @@ cap = cv2.VideoCapture(0)
 # Loop until the user presses the Esc key
 while True:
     # Capture a frame from the camera
-    ret, frame = cap.read()
-    results = model.predict(source=frame, classes=0) # person class
+    success, frame = cap.read()
+    results = model.predict(source=frame, classes=[0,1]) # person class 0
 
     # If the frame is empty, break out of the loop
-    if not ret:
+    if not success:
         break
 
-    for result in results:
-        print (result)
+    try: 
+        for result in results:
+            print (result)
 
-        # Segmentation
-        data = result.masks.data      # masks, (N, H, W)
-        mask = data[0]
-        mask = mask.cpu()*255
-        mask = mask.numpy()
-        mask = mask.astype(np.uint8)
-        # num_zeros = np.count_nonzero(mask == 0)
-        detected_area = np.count_nonzero(mask == 255)
-        # print(num_zeros)
-        frame = cv2.putText(frame, f'detected_area: {detected_area}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX , 1, (255, 0, 0) , 2, cv2.LINE_AA) 
+            # Segmentation
+            data = result.masks.data      # masks, (N, H, W)
+            mask = data[0]
+            mask = mask.cpu()*255
+            mask = mask.numpy()
+            mask = mask.astype(np.uint8)
+            # num_zeros = np.count_nonzero(mask == 0)
+            detected_area = np.count_nonzero(mask == 255)
+            # print(num_zeros)
+            # frame = cv2.putText(frame, f'detected_area: {detected_area}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX , 1, (255, 0, 0) , 2, cv2.LINE_AA) 
+            frame = cv2.putText(mask, f'detected_area: {detected_area}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX , 1, (255, 0, 0) , 2, cv2.LINE_AA) 
 
-    # Display the frame
-    cv2.imshow('detected_area', frame)
+        # Display the frame
+        cv2.imshow('detected_area', frame)
 
-    # Wait for a key press
-    key = cv2.waitKey(1)
+        # Wait for a key press
+        key = cv2.waitKey(1)
 
-    # If the user presses the Esc key, break out of the loop
-    if key == 27:
-        break
+        # If the user presses the Esc key, break out of the loop
+        if key == 27:
+            break
 
-# Release the camera
-cap.release()
+    except: 
+        print('please check cameras and objects to be inspected')
+
+        # Release the camera
+        cap.release()
 
 # Close all windows
 cv2.destroyAllWindows()
